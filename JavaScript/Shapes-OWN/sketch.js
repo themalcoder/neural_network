8e1c640dce9c3bcc28294403bf8c864735677ec1
themalcoder <themalcoder@gmail.com>
2021-04-11 03:03:47 +0530

    Try to get the input from the user.

--- JavaScript/Shapes-OWN/sketch.js
+++ JavaScript/Shapes-OWN/sketch.js
@@ -112,20 +112,52 @@ function setup() {
   // Making the Neural Network
   nn = new NeuralNetwork(784, 64, 3); //28 * 28 dimensions
 
-  trainData();
+  nn.setLearningRate(0.01); // learning rate of 0.01 works best for this.
+  trainData(20);
+
+  // console.log("Running the tests");
+  // testData(testingData);
 }
 
 function trainData(epochs = 1) {
+  
+  shuffle(trainingData, true);
+
   console.log(`Training for ${epochs} epochs!`);
   for(let e = 0; e < epochs; e++) {
-    for(let i = 0; i < trainData.length; i++) {
+    for(let i = 0; i < trainingData.length; i++) {
       let input = trainingData[i].data;
       let target = trainingData[i].target;
       nn.train(input, target);
     }
     console.log(`Epoch : ${e + 1}`);
+    testData(testingData);
+    console.log("");
   }
   console.log('Training done!');
+}
+
+function testData(testing) {
+  let correct = 0;
+  for(let i = 0; i < testing.length; i++) {
+    let input = testing[i].data;
+    let target = testing[i].target;
+
+    let guess = nn.feedForward(input);
+    let m = max(guess);
+    let classification = guess.indexOf(max(m));
+    let label = target.indexOf(1);
+    // console.log(guess);
+    // console.log(classification);
+    // console.log(target);
+
+    if(classification === label) {
+      correct++;
+    }
+
+  }
+  let percent = nf(100 * correct / testing.length, 2, 2);
+  console.log(`Correct : ${percent}%`);
 }
 
 
@@ -141,7 +173,7 @@ function gotResults(err, results) {
 
   let label = results[0].label;
   let confidence = nf(100 * results[0].confidence, 2, 0);
-  resultsDiv.html(`${label} ${confidence}%`);
+  resultsDiv.html(`${label} ${confidence} %`);
   // console.log(results);
   classifyImage();
 }
